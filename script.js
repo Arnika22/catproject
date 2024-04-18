91e3369ad468b6d45c27c63aed4f53073aa485ef
Arnika22 <arnika22@navgurukul.org>
2024-04-18 23:38:22 +0530

    inicial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let head=document.getElementById("head");
+let but=document.getElementById("but");
+let but1=document.getElementById('but1')
+function pro(a){
+    // var pic;
+    if (a==0){
+        pic="bulboff.png"
+        photo="cat-eyes-img.png"
+        head.textContent="Switch OFF"
+        but1.style.backgroundColor="rgb(179, 181, 183)"
+        but.style.backgroundColor="green"
+    }
+    else{
+        pic="bulbon.png"
+        photo="cat-img.png"
+        head.textContent="Switch ON"
+        but.style.backgroundColor="rgb(179, 181, 183)"
+        but1.style.backgroundColor="red"
+    }
+    document.getElementById("bol").src=pic;
+    document.getElementById("cat").src=photo;
+}
